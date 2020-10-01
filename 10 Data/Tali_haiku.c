6766a8d262012efa676e3605669d524a47e77587
Tali Izhaky <taliizhaky@gmail.com>
2020-09-30 21:20:04 -0700

    added comments

--- 10 Data/Tali_haiku.c
+++ 10 Data/Tali_haiku.c
@@ -3,7 +3,10 @@
 int main()
 {
     printf("Calm as a river\a\n");
+    //become one with the river
     printf("Tranquility in my heart\a\n");
+    //let the river flow through the heart
     printf("Blue summer skies reign.\a\n");
+    //ahhhh summer.
     return 0;
 }
