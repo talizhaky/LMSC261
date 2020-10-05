a15a426f9a34ee0af8f326dac3a09f5caf24dbbe
Tali Izhaky <taliizhaky@gmail.com>
2020-10-05 06:56:41 -0700

    maybe this works

--- 11Arrays/thinkoutside copy.c
+++ 11Arrays/thinkoutside copy.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  for (int i = 0; i < 10 ; i++)
+    //prints 8 lines
+  for (int i = 0; i < 8 ; i++)
   {
-      for (int j = 0 ; j < 10 ; j++)
+      for (int j = 0 ; j < 1++ ; j++)
       {
       printf("#");
       }
