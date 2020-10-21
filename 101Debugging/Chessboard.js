d3eb3cfc4d595ee5dd6278ea869894f2651b8132
Tali Izhaky <taliizhaky@gmail.com>
2020-10-21 00:27:49 -0700

    chessboard v2

--- 101Debugging/Chessboard.js
+++ 101Debugging/Chessboard.js
@@ -1,15 +1,17 @@
-//defines size of chessboard
-var size = 8;
-const bird = " #"
-const frog = "# "
 
 //controls height of chessboard
 for (number=1; number <= size; number = number + 1) {
+
+//defines size of chessboard
+  var size = 11;
+  const bird = " #"
+  const frog = "# "
+
   if (number%2==0) {
-    console.log(bird.repeat(4));
+    console.log(bird.repeat(size/2));
   }
 
   else {
-    console.log(frog.repeat(4));
+    console.log(frog.repeat(size/2));
   }
 }
