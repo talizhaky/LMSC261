7b2a2f2e989279ed58c66cba60c56a2d3b841d15
Tali Izhaky <taliizhaky@gmail.com>
2020-11-17 21:07:33 -0800

    Update weather.py

--- Final Project/weather.py
+++ Final Project/weather.py
@@ -7,23 +7,22 @@ owm = OWM('b4422cd2b94310f148af2aa9a9649490')
 mgr = owm.weather_manager()
 reg = owm.city_id_registry()
 
-city_name = input("City Name: ")
+city = input("City Name: ")
 
-country_name = input("Country Abbreviation: ")
+country = input("Country Abbreviation: ")
 
 list_of_locations = reg.locations_for('moscow', country='RU')
 moscow = list_of_locations[0]
 lat = moscow.lat
 lon = moscow.lon
 
-"""
-takes the coordinates and finds the weather
-"""
+
+#takes the coordinates and finds the weather
 
 one_call = mgr.one_call(lat=moscow.lat, lon= moscow.lon)
 temp = one_call.forecast_daily[0].temperature('fahrenheit').get('feels_like_morn', None)
 
-print(f"{temp} degrees Fahrenheit.")
+print(f"It is {temp} degrees Fahrenheit in {city}, {country}.")
 
 if temp <= 40:
     print("You better be wearing layers. Do NOT forget a hat, scarf, and gloves!")
